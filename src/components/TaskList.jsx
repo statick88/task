ad7f87d93d58e85894b1b32e23fa0c2e2c839bd3
mvespin1 <107557153+mvespin1@users.noreply.github.com>
2024-02-01 17:54:15 -0500

    Mejora realizada: Autenticación de usuarios mediante GITHUB

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,14 +2,15 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+
 const TaskList = ({ supabase }) => {
-  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState([]);
 
   useEffect(() => {
-    async function fetchTasks() {
+    async function fetchTask() {
       try {
-        let { data: tasks, error } = await supabase
-          .from('tasks')
+        let { data: task, error } = await supabase
+          .from('task')
           .select('*')
           .order('id', { ascending: true });
 
@@ -17,19 +18,19 @@ const TaskList = ({ supabase }) => {
           throw error;
         }
 
-        setTasks(tasks);
+        setTask(task);
       } catch (error) {
-        console.error('Error fetching tasks:', error.message);
+        console.error('Error fetching task:', error.message);
       }
     }
 
-    fetchTasks();
+    fetchTask();
   }, [supabase]);
 
   async function deleteTask(id) {
     try {
-      await supabase.from('tasks').delete().eq('id', id);
-      setTasks(tasks.filter((task) => task.id !== id));
+      await supabase.from('task').delete().eq('id', id);
+      setTask(task.filter((task) => task.id !== id));
     } catch (error) {
       console.error('Error deleting task:', error.message);
     }
@@ -37,15 +38,17 @@ const TaskList = ({ supabase }) => {
 
   return (
     <div>
-      <h2>Task List</h2>
+      <center>
+      <h2 >Task List</h2>
       <ul>
-        {tasks.map((task) => (
+        {task.map((task) => (
           <li key={task.id}>
             {task.title}
             <button onClick={() => deleteTask(task.id)}>Delete</button>
           </li>
         ))}
       </ul>
+      </center>
     </div>
   );
 };
